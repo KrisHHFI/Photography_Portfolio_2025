fe06b5b10c7fb7cf2111e733b94c8e3be92ab873
KrisHHFI <kristopherpepperuk@gmail.com>
2025-02-17 15:15:48 +0200

    Deploy to GitHub pages 4

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/Photography_Portfolio_2025',
-  assetPrefix: '/Photography_Portfolio_2025',
+  basePath: isProd ? '/Photography_Portfolio_2025' : '',
+  assetPrefix: isProd ? '/Photography_Portfolio_2025' : '',
 };
 
 export default nextConfig;
