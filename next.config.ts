28fe3709efda927c6205e85cc191228d1d2e2b79
KrisHHFI <kristopherpepperuk@gmail.com>
2025-02-17 15:05:28 +0200

    Deploy to GitHub pages 3

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  
+  basePath: '/Photography_Portfolio_2025',
+  assetPrefix: '/Photography_Portfolio_2025',
 };
 
 export default nextConfig;
