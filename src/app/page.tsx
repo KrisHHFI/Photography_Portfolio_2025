27e762ede10ad038d47e41f2862b7e5ecac494cd
KrisHHFI <kristopherpepperuk@gmail.com>
2025-02-17 19:50:24 +0200

    Removed the default app content which was returned to the screen 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import styles from "./page.module.css";
 import Signature from "../components/Signature";
 
 export default function Home() {
-  const assetPrefix = process.env.NODE_ENV === 'production' ? '/Photography_Portfolio_2025' : '';
+  //const assetPrefix = process.env.NODE_ENV === 'production' ? '/Photography_Portfolio_2025' : '';
 
   return (
     <>
